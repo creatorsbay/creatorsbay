25107b996ae845e6377d42fac4b8766132b447b7
creatorsbay <91531645+creatorsbay@users.noreply.github.com>
2021-10-04 09:56:35 +0530

    first commit

---
+++ insert.php
@@ -0,0 +1,38 @@
+
+<?php
+
+		// servername => localhost
+		// username => root
+		// password => empty
+		// database name => staff
+		$conn = mysqli_connect("localhost", "root", "", "staff");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking Email value from the input
+		
+		$email = $_REQUEST['email'];
+		
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO college VALUES ('$email')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>data stored in a database successfully."
+				. " Please browse your localhost php my admin"
+				. " to view the updated data</h3>";
+
+			echo nl2br("$email");
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+?>
+
